480cfb482a6d7305cd6941732c4f68944f3ac1e8
shenliang34 <971935832@qq.com>
2015-07-10 22:12:23 +0800

    增加小鸟随机飞，判断结束

--- Bird/src/GameScene.js
+++ Bird/src/GameScene.js
@@ -7,12 +7,14 @@ var GameLayer = cc.Layer.extend({
 	_time:null,//用于显示时间
 	
 	// 640 480
+	_birdLayer:null,
 	_birdRect:[
 	           cc.rect(0,0,320,240),
 	           cc.rect(320,0,320,240),
 	           cc.rect(0,240,320,240),
 	           cc.rect(320,240,320,240)
 	           ],
+	_birdList:null,
 	ctor:function(){
 		this._super();
 		var size = cc.winSize;
@@ -25,7 +27,7 @@ var GameLayer = cc.Layer.extend({
 		//初始化数据
 		this._scoreValue = 0;
 		this._countDown = 10000;
-		
+		this._birdList = [];
 		//分数
 		this._score = new cc.LabelTTF("0","",50);
 		this._score.setAnchorPoint(0, 1);//左上角
@@ -38,11 +40,17 @@ var GameLayer = cc.Layer.extend({
 		this._time.setPosition(size.width-10,size.height - 10);
 		this.addChild(this._time);
 		
+		//
+		this._birdLayer = new cc.Node();
+		this.addChild(this._birdLayer);
 		return true;
 	},
 	onEnter:function(){
 		this._super();
 		this._step = 0;
+		this._countDown = 10000;
+		this._score = 0;
+		
 		this.scheduleUpdate();
 	},
 	onExit:function(){
@@ -54,16 +62,39 @@ var GameLayer = cc.Layer.extend({
 		
 		if(this._countDown<=0){
 			this.unscheduleUpdate();
+			cc.director.pushScene(new cc.TransitionFade(1,new OverScene()))//new OverScene())
 		}
 		else{
 			var size = cc.winSize;
-			this._countDown -= dt*1000;
+			this._countDown -= (dt*1000)%10;
 			this._time.setString(Math.floor(this._countDown/1000)+"'");
 			if(this._step%20 == 0){
 				//建立小鸟
 				var random = Math.random()>0.5?1:-1;
-				var startPosition = cc.p(random>0.5?size.width:0, Math.random()*size.height);
-				var endPosition = cc.p(random>0.5?0:size.width, Math.random()*size.height);
+				var startPosition = cc.p(random>0.5?size.width+100:0-100, Math.random()*size.height);
+				var endPosition = cc.p(random>0.5?0-100:size.width+100, Math.random()*size.height);
+				
+				var time = 1+Math.floor(Math.random()*3);
+				var bird = new cc.Sprite(res.Bird_png,this._birdRect[Math.floor(Math.random()*4)]);
+				bird.setScale(-0.5*random, 0.5);
+				bird.setAnchorPoint(0, 0.5);
+				bird.setPosition(startPosition);
+				this._birdLayer.addChild(bird);
+				
+				this._birdList.push(bird);
+				var self = this;
+				bird.runAction(
+						new cc.Sequence(new cc.MoveTo(time,endPosition),
+						new cc.CallFunc(function(){
+							for (var int = 0; int < self._birdList.length; int++) {
+								if(self._birdList[i] == bird){
+									self._birdList.splice(i, 1);
+									bird.removeFromParent();
+									break;
+								}
+							}
+						})));
+						
 			}
 				
 		}
