a79148b3d54a1ad98d99f421d730fdd5b9c6ed90
shenliang34 <971935832@qq.com>
2015-07-10 20:50:01 +0800

    添加引擎代码

---
+++ Bird/main.js
@@ -0,0 +1,11 @@
+cc.game.onStart = function(){
+    cc.view.adjustViewPort(false);
+    cc.view.setDesignResolutionSize(1024, 768, cc.ResolutionPolicy.FIXED_WIDTH);
+    cc.view.resizeWithBrowserSize(true);
+    //load resources
+    cc.LoaderScene.preload(g_resources, function () {
+//        cc.director.runScene(new HelloWorldScene());
+    	cc.director.runScene(new StartScene());
+    }, this);
+};
+cc.game.run();
---
+++ Bird/src/resource.js
@@ -0,0 +1,15 @@
+var res = {
+    HelloWorld_png : "res/HelloWorld.png",
+    CloseNormal_png : "res/CloseNormal.png",
+    CloseSelected_png : "res/CloseSelected.png",
+    //bird
+    Bird_png:"res/bird.png",
+    Continue_png:"res/continue.png",
+    Scene_jpg:"res/scene.jpg",
+    Start_png:"res/start.png"
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
